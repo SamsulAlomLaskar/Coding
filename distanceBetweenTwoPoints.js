045fb0422b332a29b32bfce6bc40b0c79c649849
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-03-14 20:12:20 +0530

    currying fucntion pushing the modified file of yesterday

--- distanceBetweenTwoPoints.js
+++ distanceBetweenTwoPoints.js
@@ -1,9 +1,8 @@
 //? Distance between two points using arrow function
 
-const distance = (startDistance, endDistance) =>
+const distance = (start, end) =>
   +Math.sqrt(
-    Math.pow(endDistance.x - startDistance.x, 2) +
-      Math.pow(endDistance.y - startDistance.y, 2)
+    Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2)
   ).toFixed(2);
 
 console.log(distance({ x: 2, y: 2 }, { x: 11, y: 8 }));
