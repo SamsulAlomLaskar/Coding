64564d48a7cb45e00e9de69c027b22a51f43b105
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-04-14 20:12:20 +0530

    one edit for push nothing to push

--- zakatTracker.js
+++ zakatTracker.js
@@ -7,3 +7,9 @@ const zakatCalculator = (zakataRate, zakatAmount) => {
 console.log(zakatCalculator(2500, 534023));
 const check = zakatCalculator(2500, 329083);
 console.log(check);
+
+//* =====================
+
+let num = [1, 2, 3];
+num[3] = num;
+console.log(num);
