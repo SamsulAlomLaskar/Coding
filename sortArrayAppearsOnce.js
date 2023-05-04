16016c1e0f35dfd0bb29eea529ef4cb330d46cd0
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-05-04 20:12:20 +0530

    method 4 Time Complexity: O(Log n)

--- sortArrayAppearsOnce.js
+++ sortArrayAppearsOnce.js
@@ -88,7 +88,7 @@ function search2(arr, low, high) {
   if (low > high) return;
 
   if (low == high) {
-    document.write("The required element is " + arr[low]);
+    console.log("The required element is", arr[low]);
     return;
   }
 
@@ -99,14 +99,14 @@ function search2(arr, low, high) {
   // same as mid, then output element lies on
   // right side, else on left side
   if (mid % 2 == 0) {
-    if (arr[mid] == arr[mid + 1]) search(arr, mid + 2, high);
-    else search(arr, low, mid);
+    if (arr[mid] == arr[mid + 1]) search2(arr, mid + 2, high);
+    else search2(arr, low, mid);
   }
 
   // If mid is odd
   else {
-    if (arr[mid] == arr[mid - 1]) search(arr, mid + 1, high);
-    else search(arr, low, mid - 1);
+    if (arr[mid] == arr[mid - 1]) search2(arr, mid + 1, high);
+    else search2(arr, low, mid - 1);
   }
 }
 
@@ -115,3 +115,82 @@ var arr2 = [1, 1, 2, 4, 4, 5, 5, 6, 6];
 var len2 = arr2.length;
 
 search2(arr2, 0, len2 - 1);
+
+//! =======================Method 4===============================
+
+/* 
+Time Complexity: O(Log n)
+Auxiliary Space: O(1) 
+
+Note: Other Solutions to the question are slight variations of the approaches discussed in this post.
+
+Another Approach: An Efficient Solution can find the required element in O(Log n) time. The idea is to use Binary Search without recursion. All elements before the required have the first occurrence at even index (0, 2, ..and so on) and the next occurrence at odd index (1, 3, ..and so on). 
+
+The approach will be as follows:
+
+Find the middle index assuming mid using start pointer and end pointer. And check the mid element in the following cases
+
+Case 1) If mid element is not equal to mid+1 element  and mid-1 element. This case returns the answer.
+Case 2) When mid element is even and equal to mid+1 element this means number is not present in the left side of the array. In this case start pointer will change to mid+1.
+Case 3) When mid element is odd and equal to mid-1 element this means number is not present in the left side of the array. In this case start pointer will change to mid+1.
+Case 4) When mid element is odd and equal to mid+1 element this means number is not present in the right side of the array. In this case end pointer will change to mid-1.
+Case 5) When mid element is even and equal to mid-1 element this means number is not present in the right side of the array. In this case end pointer will change to mid-1. 
+Check for all case for possible values of mid till start<=end..
+
+If all checks fail there is no such element.
+
+This solution requires extra checks for edge cases.
+
+Edge Case 1) If only one element is present in the array. Therefore return the only element of the array.
+Edge Case 2) If last element of the array is the required element. Therefore return the last element of the array.
+Edge Case 3) If first element of the array is the required element. Therefore return the first element of the array.
+*/
+
+function search3(nums) {
+  // A Binary Search based method to find the element
+  // that appears only once
+  var start = 0,
+    end = nums.length - 1,
+    mid;
+
+  // For Edge Cases
+  if (nums.length == 1)
+    // If only one element is in the array
+    return nums[0];
+
+  if (nums[start] != nums[start + 1])
+    // If the first element
+    // is the element that
+    // appears only once
+    return nums[start];
+
+  if (nums[end] != nums[end - 1])
+    // If Last element is the element
+    // that appears only once
+    return nums[end];
+
+  // Binary Search
+  while (start <= end) {
+    mid = start + (end - start) / 2;
+
+    // CASE 1
+    if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+      return nums[mid];
+    // CASE 2 and CASE 3
+    else if (
+      (nums[mid] == nums[mid + 1] && mid % 2 == 0) ||
+      (nums[mid] == nums[mid - 1] && mid % 2 != 0)
+    )
+      start = mid + 1;
+    // CASE 4 and CASE 5
+    else end = mid - 1;
+  }
+
+  // If no such element found
+  return -1;
+}
+
+var arr3 = [1, 1, 2, 4, 4, 5, 5, 6, 6];
+var element = search3(arr3);
+if ((element = 2)) console.log("The required element is " + element);
+else console.log("There is no such element");
