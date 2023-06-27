360fa00f4c3e4504222e54f5570475ad211c7fe6
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-06-27 14:20:20 +0530

    oneLine file added and added a module unique elements unique elements

--- oneLiners.js
+++ oneLiners.js
@@ -1,5 +1,11 @@
 // Copy to clipboard
-const copyToClipboard = (text) =>
-  navigator.clipboard?.writeText && navigator.clipboard.writeText(text);
+// const copyToClipboard = (text) =>
+//   navigator.clipboard?.writeText && navigator.clipboard.writeText(text);
 
-copyToClipboard("Hello copy to clipboard");
+// copyToClipboard("Hello copy to clipboard");
+
+// Unique Elements
+
+const getUnique = (arr) => [...new Set(arr)];
+const arr = [1, 2, 2, 4, 5, 6, 6, 7, 7, 5, 5, 5, 8];
+console.log(getUnique(arr));
