40bf3d2be764fa0b9600beb53cbcb9cee2ee7e27
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-07-08 14:20:20 +0530

    added a new recursion example function recursion example function

--- oneLiners.js
+++ oneLiners.js
@@ -97,3 +97,14 @@ const asyncFunc = async () => {
 };
 
 asyncFunc();
+
+// Recursion example
+
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return factorial(num - 1) * num;
+}
+
+console.log(factorial(5));
